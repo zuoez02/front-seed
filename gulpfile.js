827695a6a94d53629080d6daf85e0e02abafb845
zuoez02 <zuoez02@outlook.com>
2016-04-06 18:04:50 +0800

    eslint files

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 
 gulp.task('babel', () => {
-	return gulp.src(['src/server/**/*.js','src/server/app.js'])
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(gulp.dest('src/lib'));
+  return gulp.src(['src/server/**/*.js', 'src/server/app.js'])
+    .pipe(babel({
+      presets: ['es2015'],
+    }))
+    .pipe(gulp.dest('src/lib'));
 });
 
 gulp.task('eslint', () => {
-    return gulp.src(['**/*.js', '!node_modules/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+  return gulp.src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
--- src/server/bin/start.js
+++ src/server/bin/start.js
@@ -8,32 +8,11 @@ const debug = debuggee('front-seed:server');
 import http from 'http';
 
 /**
- * Get port from environment and store in Express.
- */
-
-let port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-let server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val) {
-  let port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -49,6 +28,31 @@ function normalizePort(val) {
 }
 
 /**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? `pipe ${addr}`
+    : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+}
+
+/**
  * Event listener for HTTP server "error" event.
  */
 
@@ -57,18 +61,18 @@ function onError(error) {
     throw error;
   }
 
-  let bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  const bind = typeof port === 'string'
+    ? `Pipe ${port}`
+    : `Port ${port}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+      console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+      console.error(`${bind} is already in use`);
       process.exit(1);
       break;
     default:
@@ -76,14 +80,11 @@ function onError(error) {
   }
 }
 
+
 /**
- * Event listener for HTTP server "listening" event.
+ * Listen on provided port, on all network interfaces.
  */
 
-function onListening() {
-  let addr = server.address();
-  let bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
