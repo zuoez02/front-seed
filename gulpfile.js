e8210de2de5ac5e91fd8cd767b3ae9c3bdad187c
zuoez02 <zuoez02@outlook.com>
2016-04-06 17:47:56 +0800

    Use eslint-config-airbnb. Use babel-node to start

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 gulp.task('babel', () => {
 	return gulp.src(['src/server/**/*.js','src/server/app.js'])
@@ -7,4 +8,11 @@ gulp.task('babel', () => {
 			presets: ['es2015']
 		}))
 		.pipe(gulp.dest('src/lib'));
+});
+
+gulp.task('eslint', () => {
+    return gulp.src(['**/*.js', '!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
