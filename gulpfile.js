8b90aa8eb28b4600a3a86f55e83a2f4e6d5b6526
zuoez02 <zuoez02@outlook.com>
2016-04-06 18:07:15 +0800

    Eslint code

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,15 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 
-gulp.task('babel', () => {
-  return gulp.src(['src/server/**/*.js', 'src/server/app.js'])
+gulp.task('babel', () => gulp.src(['src/server/**/*.js', 'src/server/app.js'])
     .pipe(babel({
       presets: ['es2015'],
     }))
-    .pipe(gulp.dest('src/lib'));
-});
+    .pipe(gulp.dest('src/lib'))
+);
 
-gulp.task('eslint', () => {
-  return gulp.src(['**/*.js', '!node_modules/**'])
+gulp.task('eslint', () => gulp.src(['**/*.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
+    .pipe(eslint.failAfterError())
+);
