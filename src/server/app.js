75be1345d027ddd9ad02c250d8984ad31dd879de
zuoez02 <zuoez02@outlook.com>
2016-04-22 14:00:02 +0800

    Remove user router

--- src/server/app.js
+++ src/server/app.js
@@ -5,7 +5,6 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
 import routes from './routes/index';
-import users from './routes/users';
 
 const app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
